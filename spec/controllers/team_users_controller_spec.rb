e967d87af3d0699d712459b0574a09e1873bf054
Paulo Cesar <pcdsjr@gmail.com>
2017-06-27 08:21:10 -0500

    chore: controllers part 01

--- spec/controllers/team_users_controller_spec.rb
+++ spec/controllers/team_users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ChannelsController, type: :controller do
+RSpec.describe TeamUsersController, type: :controller do
 
   describe "GET #create" do
     it "returns http success" do
@@ -12,13 +12,6 @@ RSpec.describe ChannelsController, type: :controller do
   describe "GET #destroy" do
     it "returns http success" do
       get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
   end
