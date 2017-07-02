773abbd93723ae969d29e439a9cc516afcc8fa9b
Paulo Cesar <pcdsjr@gmail.com>
2017-07-02 12:06:09 -0500

    chore: adjusts

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,7 +3,6 @@ class TeamsController < ApplicationController
   before_action :set_by_slug_team, only: [:show]
 
   def index
-    @teams = current_user.teams
   end
 
   def show
@@ -27,8 +26,7 @@ class TeamsController < ApplicationController
     @team.destroy
 
     respond_to do |format|
-      format.json { head :no_content }
-      format.html { redirect_to main_app.root_url, notice: 'Team deleted' }
+      format.json { render json: true }
     end
   end
 
